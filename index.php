fdb7444e3127641f255a43754cf733901914d00f
rifabayuzul <rifabayuzulfanida@gmail.com>
2024-08-20 11:33:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt modi quos commodi enim quo ipsum sequi asperiores nemo id officia excepturi ducimus nesciunt, assumenda, magnam labore, consectetur aperiam quasi nihil.</p>
+
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+  </ul>
 </body>
 </html>
